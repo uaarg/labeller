aa0fc9c034aee7a9c07bfa110d7cc67e1df8ed65
Aidan Olsen <a.olsen2024@icloud.com>
2024-01-08 16:06:25 -0700

    Fix typo in example run script

--- examples/run.sh
+++ examples/run.sh
@@ -5,7 +5,7 @@ set -o errexit
 example="$1"
 
 if ! git status 2&>1 >/dev/null; then
-  echo "Please run this script from inside the labller repository"
+  echo "Please run this script from inside the labeller repository"
   exit 1
 fi
 
