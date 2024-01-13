79d9ea95412669609f468bd74c45340e42fee7b4
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2024-01-13 15:46:56 -0700

    linter and formatter

--- benchmarks/yolo.py
+++ benchmarks/yolo.py
@@ -2,7 +2,6 @@ from typing import Optional
 
 from PIL import Image
 import numpy as np
-import cv2 as cv
 import sys
 import torch
 from pathlib import Path
@@ -19,18 +18,19 @@ from models.common import DetectMultiBackend
 from utils.augmentations import letterbox
 from utils.general import non_max_suppression, check_img_size, xyxy2xywh, scale_coords
 from utils.torch_utils import select_device
-from utils.plots import Annotator
 
 
 class YoloDetector(LandingPadDetector):
-    def __init__(self, weights='yolov5s.pt',
-                imgsz=(640, 640),
-                device='cpu',
-                data='data/coco128.yaml',
-                half=False, # use FP16 half-precision inference
-                dnn=False # use OpenCV DNN for ONNX inference
-                ) -> None:
-        
+
+    def __init__(
+        self,
+        weights='yolov5s.pt',
+        imgsz=(640, 640),
+        device='cpu',
+        data='data/coco128.yaml',
+        half=False,  # use FP16 half-precision inference
+        dnn=False  # use OpenCV DNN for ONNX inference
+    ) -> None:
         """
         Loads the inference model into memory and other setup
         """
@@ -40,10 +40,10 @@ class YoloDetector(LandingPadDetector):
         # Load model
         device = select_device(device)
         model = DetectMultiBackend(weights,
-                                device=device,
-                                dnn=dnn,
-                                data=data,
-                                fp16=half)
+                                   device=device,
+                                   dnn=dnn,
+                                   data=data,
+                                   fp16=half)
         imgsz = check_img_size(imgsz, s=model.stride)  # check image size
 
         # Run inference
@@ -51,9 +51,9 @@ class YoloDetector(LandingPadDetector):
 
         self.model = model
         self.imgsz = imgsz
-        self.conf_thres=0.25 # confidence threshold
-        self.iou_thres=0.45 # NMS IOU threshold
-        self.max_det=1000 # maximum detections per image
+        self.conf_thres = 0.25  # confidence threshold
+        self.iou_thres = 0.45  # NMS IOU threshold
+        self.max_det = 1000  # maximum detections per image
 
     def predict(self, image: Image.Image) -> Optional[BoundingBox]:
         """
@@ -66,8 +66,11 @@ class YoloDetector(LandingPadDetector):
         im0 = np.array(image)
         gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
 
-        im = letterbox(im0, self.imgsz, stride=self.model.stride, auto=False,
-                    scaleup=False)[0]  # padded resize
+        im = letterbox(im0,
+                       self.imgsz,
+                       stride=self.model.stride,
+                       auto=False,
+                       scaleup=False)[0]  # padded resize
         im = im.transpose((2, 0, 1))  # HWC to CHW
         im = np.ascontiguousarray(im)  # contiguous
 
@@ -82,11 +85,11 @@ class YoloDetector(LandingPadDetector):
 
         # NMS
         pred = non_max_suppression(pred,
-                                self.conf_thres,
-                                self.iou_thres,
-                                classes=None,
-                                agnostic=False,
-                                max_det=self.max_det)
+                                   self.conf_thres,
+                                   self.iou_thres,
+                                   classes=None,
+                                   agnostic=False,
+                                   max_det=self.max_det)
 
         # Second-stage classifier (optional)
         # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
@@ -94,7 +97,7 @@ class YoloDetector(LandingPadDetector):
         results = []
         for i, prediction in enumerate(pred):
             prediction[:, :4] = scale_coords(im.shape[2:], prediction[:, :4],
-                                            im0.shape).round()
+                                             im0.shape).round()
             for *xyxy, conf, cls in reversed(prediction).tolist():
                 xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) /
                         gn).view(-1).tolist()
@@ -106,13 +109,15 @@ class YoloDetector(LandingPadDetector):
                     'w': xywh[2],
                     'h': xywh[3]
                 })
-        
+
         if not results:
             return None
 
         max_confidence = max(results, key=lambda x: x['confidence'])
-        x, y, w, h = max_confidence['x'], max_confidence['y'], max_confidence['w'], max_confidence['h']
+        x, y, w, h = max_confidence['x'], max_confidence['y'], max_confidence[
+            'w'], max_confidence['h']
         return BoundingBox(Vec2(x, y), Vec2(w, h))
+
 
 if __name__ == "__main__":
     import glob
