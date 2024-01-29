4f6f3b29a6c35fea59f356ee5ce27034ab332fca
fadilm777 <fadil.m777@gmail.com>
2024-01-28 18:22:10 -0700

    testing

--- benchmarks/yolov8.py
+++ benchmarks/yolov8.py
@@ -35,34 +35,9 @@ class Yolov8Detector(LandingPadDetector):
         self.iou_thres = 0.45
         super().__init__()
 
-    def predict(self, image: Image.Image) -> Optional[BoundingBox]:
+    def predict(self, source):
 
-        im0 = np.array(image)
-
-        # Resize and transform im0 into one the model can read
-        im, ratio, (dw, dh) = letterbox(im0,
-                                        self.imgsz,
-                                        stride=self.model.stride,
-                                        auto=False,
-                                        scaleup=False)  # padded resize
-        im = im.transpose((2, 0, 1))  # HWC to CHW
-        im = np.ascontiguousarray(im)  # contiguous
-
-        im = torch.from_numpy(im).to(self.model.device)
-        im = im.half() if self.model.fp16 else im.float()  # uint8 to fp16/32
-        im /= 255  # 0 - 255 to 0.0 - 1.0
-        if len(im.shape) == 3:
-            im = im[None]  # expand for batch dim
-        
-        
-        results = self.model.predict(im, 
-                                     conf=self.conf_thres,
-                                     iou=self.iou_thres,
-                                     imgsz=self.imgsz,
-                                     half=self.half,
-                                     device=self.device,
-                                     max_det=self.max_det,
-                                     )
+       
         print(results)
         
 if __name__ == "__main__":
@@ -71,8 +46,7 @@ if __name__ == "__main__":
     
     detector = Yolov8Detector()
     bundles = MultiBundleLoader(glob.glob("tmp/labelled-bundles/*"))
-    benchmark(detector, bundles)
-
+    print(bundles.count)
 
 
 
