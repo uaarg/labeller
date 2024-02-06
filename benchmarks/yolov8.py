85e45d94a1285b13c8ebbde244104f79b82e42d8
fadilm777 <fadil.m777@gmail.com>
2024-02-06 08:43:33 -0700

    lint fix

--- benchmarks/yolov8.py
+++ benchmarks/yolov8.py
@@ -13,12 +13,14 @@ from benchmarks.detector import BoundingBox, LandingPadDetector
 
 class Yolov8Detector(LandingPadDetector):
 
-    def __init__(self, 
+    def __init__(self,
                  weights='benchmarks/yolov8_nano.pt',
                  device='cpu',
                  imgsz = 640,
                  half = False
                  ) -> None:
+        
+        
         self.model = YOLO(weights)
         self.device = device
         self.conf_thres = 0.25
@@ -28,7 +30,6 @@ class Yolov8Detector(LandingPadDetector):
         self.iou_thres = 0.45
         super().__init__()
 
-
     def predict(self, image: Image.Image) -> Optional[BoundingBox]:
         """
         Runs the model on the supplied images
@@ -36,18 +37,11 @@ class Yolov8Detector(LandingPadDetector):
         Returns a list with dictionary items for each prediction
         """
         im = np.array(image)
-        pred = self.model.predict(im,
-                                     conf=self.conf_thres,
-                                     iou=self.iou_thres,
-                                     imgsz=self.imgsz,
-                                     half=self.half,
-                                     device=self.device,
-                                     max_det=self.max_det,
-                                     verbose=False)
+        pred = self.model.predict(im, conf=self.conf_thres, iou=self.iou_thres, imgsz=self.imgsz, half=self.half, device=self.device, max_det=self.max_det, verbose=False)
         results = []
         for r in pred:
             results = r.boxes.xywh.tolist()
-        if len(results) == 0: 
+        if len(results) == 0:
             return None
         else:
             results = results[0]
